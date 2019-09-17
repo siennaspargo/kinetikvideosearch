43a6ab0c2f617fa9612e03be2b94eb32ae0e6ab0
SiennaS <ssienna48@gmail.com>
2019-09-17 00:49:52 -0400

    ant design for list and search

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -2,12 +2,20 @@
 
 import React from 'react';
 import Videoiframe from './VideoIframe';
+import 'antd/dist/antd.css';
+import { List, Typography } from 'antd';
 
 const VideoList = ({videos , handleVideoSelect}) => {
     const renderedVideos =  videos.map((video) => {
         return <Videoiframe key={video.id.videoId} video={video} handleVideoSelect={handleVideoSelect} />
     });
 
-    return <div className='ui relaxed divided list'>{renderedVideos}</div>;
+    return <div>
+    <List size ="large"
+    bordered
+    dataSource={renderedVideos}
+    renderItem={item => <List.Item>{item}</List.Item>}
+    ><div></div></List>
+</div>
 };
 export default VideoList;
--- src/components/searchbar.js
+++ src/components/searchbar.js
@@ -1,5 +1,10 @@
 import React from 'react';
 
+import 'antd/dist/antd.css';
+import { Input } from 'antd';
+
+
+
 class Searchbar extends React.Component {
     state = {
         term: ''
@@ -16,11 +21,11 @@ class Searchbar extends React.Component {
 
     render() {
         return (
-            <div className='search-bar ui segment'>
+            <div className='search-bar ui fluid input'>
                 <form onSubmit={this.handleSubmit} className='ui form'>
                     <div className='field'>
                         <label htmlFor="video-search"></label>
-                        <input onChange={this.handleChange} name='video-search' type="text" value={this.state.term}/>
+                        <Input placeholder="search youtube" allowClear onChange={this.handleChange} name='video-search' type="text" value={this.state.term}/>
                     </div>
                 </form>
             </div>
