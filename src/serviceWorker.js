d0eb8b57a4c42bbbe558ac61728d160097916a0f
SiennaS <ssienna48@gmail.com>
2019-09-17 18:18:43 -0400

    processenv

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -1,7 +1,7 @@
 
 
 const isLocalhost = Boolean(
-  window.location.hostname === 'localhost' ||
+  window.location.hostname === 'localhost' || process.env.port
     // [::1] is the IPv6 localhost address.
     window.location.hostname === '[::1]' ||
     // 127.0.0.1/8 is considered localhost for IPv4.
